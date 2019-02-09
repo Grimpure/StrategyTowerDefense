3dc2969cb6d738fd1fa299a8b0a602b717ccc16c
Unknown <ma.van.dinther@sintlucasedu.nl>
2019-02-09 11:47:18 +0100

    Added start of 1st Unit and a few placeholders

---
+++ Strategy Tower Defense/Assets/Scripts/SpawnButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnButtons : MonoBehaviour
+{
+    //What Unit to spawn, how much it costs and the cooldown before another can be spawned
+    public GameObject friendlyUnit;
+    public int unitPrice;
+    public float unitCooldown;
+
+    public void SpawnFriendlyUnit()
+    {
+        Instantiate(friendlyUnit);
+    }
+}
---
+++ Strategy Tower Defense/Assets/Scripts/Units/BunnyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyBehavior : MonoBehaviour
+{
+
+    public Vector3 speed = new Vector3(3, 0, 0); //Speed at which the Unit Moves
+    public float health = 50; //Unit's Health
+    public float defense = 0; //Unit's damage-cancellation
+    public float attackStrength = 5; //How hard the Unit hits
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= speed * Time.deltaTime;
+
+        if(health >= 0) //Unit dies
+        {
+
+        }
+    }
+}
